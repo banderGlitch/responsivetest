6422252bd3d26de5543424993e4173dc27e75ed5
banderGlitch <nernaykumar98@gmail.com>
2025-05-30 23:13:02 +0530

    changes

--- src/app/components/header/Header.jsx
+++ src/app/components/header/Header.jsx
@@ -1,18 +1,44 @@
-import React from 'react'
+'use client'
+import React, { useState } from 'react'
 import './Header.scss'
 
+
 const Header = () => {
+
+  const [isDrawerOpen, setIsDrawerOpen] = useState(false);
+
+  const toggleDrawer = () => {
+    setIsDrawerOpen(!isDrawerOpen);
+  }
+
   return (
     <div className="main">
-        <header className="header">
-            <h1>Responsive Demo</h1>
-            <nav className="nav">
-            <a href="#">Home</a>
-            <a href="#">About</a>
-            <a href="#">Contact</a>
-        </nav>
-    </header>
+      <header className="header">
+        <div className="header__content">
+          <h1>Responsive Demo</h1>
+          <button className={`hamburger ${isDrawerOpen ? 'active' : ''}`} onClick={toggleDrawer} aria-label="Toggle navigation menu">
+            <span></span>
+            <span></span>
+            <span></span>
+          </button>
+          <nav className={`nav ${isDrawerOpen ? 'active' : ''}`}>
+            <a href="#" onClick={() => setIsDrawerOpen(false)}>Home</a>
+            <a href="#" onClick={() => setIsDrawerOpen(false)}>About</a>
+            <a href="#" onClick={() => setIsDrawerOpen(false)}>Contact</a>
+          </nav>
+        </div>
+      </header>
     </div>
+    // <div className="main">
+    //     <header className="header">
+    //         <h1>Responsive Demo</h1>
+    //         <nav className="nav">
+    //         <a href="#">Home</a>
+    //         <a href="#">About</a>
+    //         <a href="#">Contact</a>
+    //     </nav>
+    // </header>
+    // </div>
   )
 }
 
